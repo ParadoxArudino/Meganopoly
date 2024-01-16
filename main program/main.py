24803c5297b072e8be537a2436016939e4430333
ParadoxArduino <danielmb062020@gmail.com>
2024-01-16 13:07:05 +0000

    sample objects

---
+++ main program/main.py
@@ -0,0 +1,19 @@
+class card:
+    def __init__(self,ID) -> None:
+        self.ID = ID
+    
+    def getID(self):
+        return self.ID
+
+class property(card):
+    def __init__(self, ID, value) -> None:
+        super().__init__(ID)
+        self.value = value
+ 
+    def getValue(self):
+        return self.value
+
+card1 = property(input(),100)
+
+print(card1.getID())
+print(card1.getValue())
