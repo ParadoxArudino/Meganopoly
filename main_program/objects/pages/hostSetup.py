69e183a6374d81b603340a1503ae02fefff0965c
ParadoxArudino <danielmb062020@gmail.com>
2024-02-07 13:04:06 +0000

    updated host and client setup pages, adding event loop

--- main_program/objects/pages/hostSetup.py
+++ main_program/objects/pages/hostSetup.py
@@ -1,15 +1,25 @@
 from .page import Page
+import pygame
 
 class HostSetup(Page):
     def __init__(self):
         super().__init__("Host Setup")
     
     def run(self, screen):
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption(self.title)
+
         while True:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    raise SystemExit
+        
             self.execute_render(screen)
+            self.clock.tick(60)
 
     def execute_render(self, screen):
         self.global_render(screen)
+
         
     
     
