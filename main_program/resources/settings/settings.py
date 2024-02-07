549795e321f8271fbf17e19c346044dec01918db
ParadoxArudino <danielmb062020@gmail.com>
2024-02-07 13:31:31 +0000

    added colours and border constants

--- main_program/resources/settings/settings.py
+++ main_program/resources/settings/settings.py
@@ -1,3 +1,11 @@
+#Colours
+BG_COLOR = "White"
+OUTER_FILL_COLOR = "blue"
+BORDER_COLOR = "black"
+
+#Window dimensions
 WIDTH = 720
 HEIGHT = 480
-BG_COLOUR = "White"
+BORDER_WIDTH = 10
+BORDER_HEIGHT = 10 
+
