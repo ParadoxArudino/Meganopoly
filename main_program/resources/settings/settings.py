a78ce64bea498d4aca1baaac032b4f71cb8ad2a6
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-02-08 09:40:45 +0000

    Add comment and fix capitalisation in settings.py

--- main_program/resources/settings/settings.py
+++ main_program/resources/settings/settings.py
@@ -1,9 +1,10 @@
-#Colours
-BG_COLOR = "White"
+# Colours
+BG_COLOR = "white"
 OUTER_FILL_COLOR = "blue"
 BORDER_COLOR = "black"
 
-#Window dimensions
+# Window dimensions
+# This is the native resolution, which is then scaled to best fit the window size
 WIDTH = 720
 HEIGHT = 480
 BORDER_WIDTH = 10
